210d8dfb5cecb0f3cb6c5e1cdea68c3a45cb9af2
Luiz Ricardo <luizricardocordeiroalves@gmail.com>
2026-04-16 15:05:57 -0300

    TextField & DatePicker

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -17,6 +17,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
